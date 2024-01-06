a48d9b9d5af68908fa4c0427f1eb0d5369b81a7b
Sumit kumar singh <151512932+Thesumitkrsingh@users.noreply.github.com>
2024-01-06 23:08:57 +0530

    Add files via upload

---
+++ Tribute_page/comming soon page/script.js
@@ -0,0 +1,20 @@
+// Set the date we're counting down to
+var countDownDate = new Date("dec 23, 2023 00:00:00").getTime();
+
+// Update the count down every 1 second
+var countdownFunction = setInterval(function() {
+    var now = new Date().getTime();
+    var distance = countDownDate - now;
+
+    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    document.getElementById("countdown").innerHTML = days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
+
+    if (distance < 0) {
+        clearInterval(countdownFunction);
+        document.getElementById("countdown").innerHTML = "EXPIRED";
+    }
+}, 1000);
